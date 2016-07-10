e5af157825a6b0247a8b9d9712b5898d65352786
Marceli.no <me@marceli.no>
2016-07-10 16:47:53 -0400

    test: Implement code coverage for server side testing
    This commit configures coverage through "nutra" and "nutra-coverage".

--- nutra.config.js
+++ nutra.config.js
@@ -4,12 +4,18 @@ module.exports = function( config ) {
         files: ['core/server/test/specs/**/*.js', 'src/**/*.js'],
         preprocessors: {
             'core/server/test/specs/**/*.js': ['nutra-babel'],
-            'src/**/*.js': ['nutra-babel']
+            'src/**/*.js': ['nutra-babel', 'nutra-coverage']
         },
-        reporters: ['nutra-minimal-reporter'],
+        reporters: ['nutra-minimal-reporter', 'nutra-coverage'],
         babelOptions: {
             configFile: './.babelrc'
+        },
+        coverageOptions: {
+            dir : 'core/server/test',
+            reporters: [
+                { type: 'html', subdir: 'coverage' },
+                { type: 'lcovonly', subdir: 'coverage', file: 'lcov.info' }
+            ]
         }
     })
 }
-
